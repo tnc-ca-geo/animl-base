bd70208d92a79d9a1b15c68bdd6f5229da595d4f
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2020-03-15 20:29:51 -0700

    Fix typo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 var config = require('./config/index');
 const chokidar = require('chokidar');
 
+console.log('Starting Animl Base. Watching directory: ', config.imgDir);
+
 // Initialize watcher
 const watcher = chokidar.watch(config.imgDir, {
   ignored: /(^|[\/\\])\../, // ignore dotfiles
   persistent: true,
-
 });
 
 // Add event listeners
 watcher
-  .on('ready', () => log('Initial scan complete. Ready for changes'))
-  .on('add', path => log(`File ${path} has been added`))
-  .on('error', error => log(`Watcher error: ${error}`));
+  .on('ready', () => console.log('Initial scan complete. Ready for changes'))
+  .on('add', path => console.log(`File ${path} has been added`))
+  .on('error', error => console.log(`Watcher error: ${error}`));
