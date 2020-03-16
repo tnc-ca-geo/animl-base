a54f3d67dc1cd68f134a945b0e85bafb0203c63b
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2020-03-15 22:42:23 -0700

    Fix typo

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   aws: {
     bucket: process.env.DEST_BUCKET,
     region: process.env.REGION,
-    accessKeyID: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 }
