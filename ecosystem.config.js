d62ef911558e4c21ec0ee6e8b771fc3e6d91a269
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2020-08-07 12:58:04 -0700

    Update PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,9 +17,10 @@ module.exports = {
     },
     {
       name: 'multibase-server-edition',
-      script: 'mbasectl -s',
+      // script: 'mbasectl -s',
+      script: '/usr/local/mbse/mbasectl',
+      args: '-s',
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      // args: 'one two',
       instances: 1,
       autorestart: true,
       exp_backoff_restart_delay: 100,
