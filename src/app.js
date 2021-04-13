12b5182f8c0dc73faa511ad1418c573164804531
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-04-13 12:57:55 -0700

    Parse mbase logs pics counter events

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const chokidar = require('chokidar');
+const moment = require('moment');
 const Tail = require('tail').Tail;
 const Queue = require('./utils/queue');
 const Worker = require('./utils/worker');
@@ -57,7 +58,24 @@ async function start() {
   try {
     let logWatcher = new Tail(config.logFile);
     logWatcher.on('line', (data) => {
-      console.log(data);
+      console.log('new line on log watcher: ', data);
+      if (data.includes('pics counter')) {
+
+        // Parse pics counter event
+        console.log('parsig pics counter event: ', data);
+        let re = new RegExp(/\d+/);
+        const event = data.split(':EVENT:');
+        const timestamp = moment(event[0]);
+        const msg = event[1].split(',');
+        const camera = msg[0].match(re);
+        const counter = msg[1].match(re);
+
+        console.log(`timestamp: ${timestamp}`);
+        console.log(`camera: ${camera}`);
+        console.log(`count: ${counter}`);
+        
+        // TODO: push to cloudwatch metrics
+      }
     });
   } catch (err) {
     console.log(`logWatcher error: ${err}`);
