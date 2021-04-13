ff1b0e3df33dd6ee8438319b1fe952621680fa56
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-04-13 14:14:25 -0700

    Publish mbase pic count events to cloudwatch

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const chokidar = require('chokidar');
-const moment = require('moment');
-const Tail = require('tail').Tail;
 const Queue = require('./utils/queue');
 const Worker = require('./utils/worker');
 const Multibase = require('./utils/multibase');
+const LogWatcher = require('./utils/logWatcher');
 const config = require('./config/index');
 
 function shutDown(code, imgWatcher, logWatcher, worker, mbase) {
@@ -12,7 +11,7 @@ function shutDown(code, imgWatcher, logWatcher, worker, mbase) {
   mbase.stop();
   worker.stop();
   imgWatcher.close().then(() => console.log('Closed'));
-  logWatcher.unwatch();
+  logWatcher.stop();
 }
 
 function validateFile(filePath) {
@@ -55,31 +54,8 @@ async function start() {
   worker.poll();
 
   // Initialize log watcher
-  try {
-    let logWatcher = new Tail(config.logFile);
-    logWatcher.on('line', (data) => {
-      console.log('new line on log watcher: ', data);
-      if (data.includes('pics counter')) {
-
-        // Parse pics counter event
-        console.log('parsig pics counter event: ', data);
-        let re = new RegExp(/\d+/);
-        const event = data.split(':EVENT:');
-        const timestamp = moment(event[0]);
-        const msg = event[1].split(',');
-        const camera = msg[0].match(re);
-        const counter = msg[1].match(re);
-
-        console.log(`timestamp: ${timestamp}`);
-        console.log(`camera: ${camera}`);
-        console.log(`count: ${counter}`);
-        
-        // TODO: push to cloudwatch metrics
-      }
-    });
-  } catch (err) {
-    console.log(`logWatcher error: ${err}`);
-  }
+  let logWatcher = new LogWatcher(config);
+  await logWatcher.init();
 
   // Clean up & shut down
   process.on('SIGTERM', (code) => {
---
+++ src/utils/logWatcher.js
@@ -0,0 +1,80 @@
+const moment = require('moment');
+const Tail = require('tail').Tail;
+const AWS = require('aws-sdk');
+const config = require('./config/index');
+
+class S3Service {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async init() {
+    AWS.config.logger = console;
+    AWS.config.update({ region: this.config.region });
+    this.cloudwatch = new AWS.CloudWatch({ apiVersion: '2010-08-01' });
+    try {
+      this.tail = new Tail(this.config.logFile);
+      this.tail('line', (data) => {
+        console.log('new line on log watcher: ', data);
+        if (data.includes('pics counter')) {
+          await this.handlePicCounterEvent(data);
+        }
+      });
+    } catch (e) {
+      console.log('Error watching multibase log file: ', e);
+    }
+  }
+
+  parsePicCountEvent(data) {
+    console.log('parsig pic counter event: ', data);
+    let re = new RegExp(/\d+/);
+    const event = data.split(':EVENT:');
+    const timestamp = moment(event[0], 'YYYY/MM/DD hh:mm:ss');
+    const msg = event[1].split(',');
+    const camera = msg[0].match(re);
+    const picCount = msg[1].match(re);
+    return { timestamp, camera, picCount }
+  }
+
+  async handlePicCounterEvent(data) {
+    // Parse event
+    const event = this.parsePicCountEvent(data);
+    console.log(`timestamp: ${event.timestamp}`);
+    console.log(`camera: ${event.camera}`);
+    console.log(`count: ${event.picCount}`);
+
+    try {
+      // Push to cloudwatch metrics
+      var params = {
+        MetricData: [
+          {
+            MetricName: 'PicCount',
+            Dimensions: [
+              {
+                Name: 'camera',
+                Value: event.camera,
+              },
+            ],
+            // StorageResolution: '5',
+            Timestamp: event.timestamp,
+            Unit: 'Count',
+            Value: event.picCount,
+          },
+        ],
+        Namespace: 'Animl',
+      };
+      return await cloudwatch.putMetricData(params).promise();
+    } catch (e) {
+      console.log('Error pushing pic counter metric to cloudwatch: ', e)
+    }
+  }
+
+  stop() {
+    console.log('Stopping logWatcher');
+    if (this.tail) {
+      this.tail.unwatch();
+    }
+  }
+}
+
+module.exports = S3Service;
