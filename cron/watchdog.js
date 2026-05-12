f864d6fc43c31755b61e755755b1ca26cfd95cd8
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2026-05-12 12:59:11 -0700

    remove unused dep

--- cron/watchdog.js
+++ cron/watchdog.js
@@ -8,24 +8,22 @@
 const https = require('https');
 const os = require('os');
 const config = require('./config/index');
-const { InventoryConfigurationFilterSensitiveLog } = require('@aws-sdk/client-s3');
 
 /**
  * Send a POST query every day
  */
 async function start() {
-
   if (!config.watchdogEnabled) {
     return;
   }
 
-  const subscriptions = config.watchdogSubscriptions.split(',')
+  const subscriptions = config.watchdogSubscriptions.split(',');
 
   const data = JSON.stringify({
     type: 'tnc',
     device_id: `animl-computer-${os.hostname()}`,
     label: config.watchdogLabel,
-    subscriptions: subscriptions
+    subscriptions: subscriptions,
   });
   const options = {
     host: 'api-dev.iotwatchdog.org',
