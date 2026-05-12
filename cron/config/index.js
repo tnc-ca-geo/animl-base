4997f527c688eca00324a853b3e3d9478ac4baf6
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2026-05-12 12:52:14 -0700

    fix config typo

--- cron/config/index.js
+++ cron/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
   queueDir: process.env.QUEUE_DIR,
   archiveDir: process.env.ARCHIVE_DIR,
   diskLimit: process.env.DISK_LIMIT_PERCENT || 60,
-  diskMountPath: process.env.DISK_MOUNTING_PATH || '/',
+  diskMountPath: process.env.DISK_MOUNT_PATH || '/',
   // env variables are always strings and they must be elevated as such
   deleteQueueForDisk: process.env.DELETE_QUEUE_FOR_DISK == 'true',
   // for testing and foo image generation
@@ -21,5 +21,5 @@ module.exports = {
   // watchdogApiEndPoint: 'api.iotwatchdog.org',
   watchdogXApiKey: process.env.WATCHDOG_X_API_KEY,
   watchdogSubscriptions: process.env.WATCHDOG_SUBSCRIPTIONS,
-  watchdogLabel: process.env.WATCHDOG_LABEL
+  watchdogLabel: process.env.WATCHDOG_LABEL,
 };
